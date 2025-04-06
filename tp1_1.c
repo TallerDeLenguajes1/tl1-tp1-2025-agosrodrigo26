9421e6cd9733200a76e17188344b44f065130200
“Agostina” <ballet1agostina@gmail.com>
2025-04-06 17:31:06 -0300

    Cambios en tp1_1.c

--- tp1_1.c
+++ tp1_1.c
@@ -2,7 +2,25 @@
 
 int main(){
 
+    int num = 10;
+    int *puntero = &num; //apunta a un entero
+
+    printf("Contenido del puntero (Valor al que apunta): %d\n", *puntero);
+    printf("Dirección de memoria a la que apunta el puntero: %p\n", puntero);
+    printf("Direccion de memoria de la variable: %p\n", &num);
+    printf("Direccion de memoria del puntero: %p\n", *puntero);
+    
+    printf("Tamanio de la variable (en bytes): %zu\n", sizeof(num));
+
+    
+    printf("Tamanio del puntero (en bytes): %zu\n", sizeof(puntero));
+
+ 
+    printf("Tamanio de un puntero a entero (int*): %zu\n", sizeof(int*));
     printf("Hola mundo");
+
+
     return 0;
+
 
 }
